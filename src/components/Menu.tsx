b8893fa7e8fc61965e6d56714ec36d135528b73f
Hithesh Sathian <hithesh.sathian@fiserv.com>
2023-11-15 16:28:32 -0500

    Pizza-shop front-end since 11/15

---
+++ src/components/Menu.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, {useState} from 'react'
+import Image from "next/image";
+import Link from 'next/link';
+
+const links = [
+    {id: 1, title: "Homepage", url: "/"},
+    {id: 2, title: "Menu", url: "/menu"},
+    {id: 3, title: "Orders", url: "/"},
+];
+
+const Menu = () => {
+    const [open,setOpen] = useState(false)
+    const user = false
+    return (
+        <div>
+            {!open ? (
+            <Image
+            src="/open.png"
+            alt=""
+            width={20}
+            height={20}
+            onClick = {() =>setOpen(true)}/>
+            ) : (<Image
+                src="/close.png"
+                alt=""
+                width={20}
+                height={20}
+                onClick = {() =>setOpen(false)}/>
+            )}
+            <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text text-3xl z-10">
+                {links.map((item) => (
+                    <Link href = {item.url} key = {item.id}>
+                        {item.title}
+                    </Link>
+                ))}
+                {!user ? (<Link href="/login">Login</Link>):(
+                <Link href="/orders">Orders</Link>)}
+            </div>
+            </div>
+    );
+};
+
+export default Menu;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Menu from './Menu';
+import Link from 'next/link';
+
+const Navbar = () => {
+    return (
+        <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
+            {/* LOGO */}
+            <div className="text-xl">
+                <Link href ="/">Pizza Shop</Link>
+            </div>
+            {/* MOBILE MENU */}
+            <div>
+                <Menu/>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
